497bdffd22799823a083846ea3cf890cded71012
Victoria Zhizhonkova <indarklight@gmail.com>
2023-06-09 17:43:44 +0300

    Rework gh actions

---
+++ .github/gitflow/start_job.js
@@ -0,0 +1,27 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  const token = process.env.GITHUB_TOKEN;
+  const gh = github.getOctokit(token);
+
+  const { owner, repo } = github.context.repo;
+
+  await gh.request(
+    "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+    {
+      owner,
+      repo,
+      workflow_id: "WORKFLOW_ID",
+      ref: "main",
+      inputs: {
+        type: "patch",
+      },
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+}
+
+void run();
